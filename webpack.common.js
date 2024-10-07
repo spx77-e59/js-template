a6b5a2854855e613492c1d0ac37f2708518c5269
somtsho <179671017+spx77-e59@users.noreply.github.com>
2024-10-07 09:37:02 +0600

    Add babel loader

--- webpack.common.js
+++ webpack.common.js
@@ -13,8 +13,22 @@ module.exports = {
       template: "./src/template.html",
     }),
   ],
+  stats: {
+    loggingDebug: ["babel-loader"],
+  },
   module: {
     rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
